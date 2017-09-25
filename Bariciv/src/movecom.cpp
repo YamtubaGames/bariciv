9a1405ef13d91c5c1cc205459a1f2af4f0f69a99
duke11d <deneveandrew@gmail.com>
2017-09-24 19:56:29 -0500

    Changed letters to represent the columns and numbers to represent rows. Also allowed lowercase letters.

--- Bariciv/src/movecom.cpp
+++ Bariciv/src/movecom.cpp
@@ -12,15 +12,15 @@ movecom::movecom(std::string tile, int numberof, int direction)
     std::string hello[30] = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30"};
     for(int i = 0; i < 30; i++) {
         if (b == hello[i]) {
-            column = i;
+            row = i;
             i = 30;
         };
     };
-    std::string hi[26] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
-    for(int i = 0; i < 26; i++) {
+    std::string hi[52] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+    for(int i = 0; i < 52; i++) {
         if (a == hi[i]) {
-            row = i;
-            i = 26;
+            column = i % 26;
+            i = 52;
         };
     };
 }
